182aa867c77506f8a9713ee27f31169dc6bf0683
Dan Beneventano <danbeneventano@protonmail.com>
2020-07-27 12:52:14 -0400

    Add iphone splash screens, nativeUI, apple status bar black-translucent

--- nuxt.config.js
+++ nuxt.config.js
@@ -148,6 +148,67 @@ export default {
         type: 'image/png',
         sizes: '16x16',
         href: '/favicon-16x16.png'
+      },
+      // APPLE SPLASHSCREENS
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphone5_splash.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphone6_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphoneplus_splash.png',
+        media:
+          '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphonex_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphonexr_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/iphonexsmax_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/ipad_splash.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/ipadpro1_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/ipadpro3_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'splashscreens/ipadpro2_splash.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
       }
     ]
   },
@@ -200,7 +261,10 @@ export default {
       name: 'Comet',
       description: 'Create and browse posts and comments on Comet',
       theme_color: '#202124',
-      favicon: false
+      favicon: false,
+      mobileAppIOS: true,
+      appleStatusBarStyle: 'black-translucent',
+      nativeUI: true
     },
     icon: {
       purpose: ['any', 'maskable']
